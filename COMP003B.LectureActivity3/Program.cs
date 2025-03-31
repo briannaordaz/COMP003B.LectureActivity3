768c2b5ab9aca60351dea43e3f71dfef0a4bdac8
briannaordaz <briannaordaz5@gmail.com>
2025-03-31 16:57:42 -0700

    added author, course, faculty, and purpose

---
+++ COMP003B.LectureActivity3/Program.cs
@@ -0,0 +1,42 @@
+/*
+ Author: Brianna Ordaz
+ Course: COMP003B: ASP.NET Core
+ Faculty: Jonathan Cruz
+ Purpose: Demonstrates routing, model binding, and validation in ASP.NET Core MVC
+ */
+
+namespace COMP003B.LectureActivity3;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
+}
